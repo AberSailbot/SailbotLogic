1a82ce0139bd0c0f436329d73bccc89a8ae39105
Kamil Mrowiec <kaamilm@gmail.com>
2013-06-06 15:30:04 +0100

    Changed  sail values

--- src/main/Boat.java
+++ src/main/Boat.java
@@ -428,13 +428,13 @@ public class Boat{
                 if(relativeWind >= 290)
                     sailPosition = 0;
                 else if(relativeWind >= 280)
-                    sailPosition = 342;
+                    sailPosition = 18; //342
                 else if(relativeWind >= 270)
-                    sailPosition = 324;
+                    sailPosition = 36; //324
                 else if(relativeWind >= 250)
-                    sailPosition = 306;
+                    sailPosition = 54; //306
                 else
-                    sailPosition = 288;
+                    sailPosition = 72; //288
             }
             System.out.println("I am setting the sail to " + sailPosition);
             com.sendMessage("set sail " + sailPosition);
