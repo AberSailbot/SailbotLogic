731a6e13dae4736ab9233360291a43cbfce505b5
thip <david@thecappers.co.uk>
2013-08-19 13:03:20 +0100

    Added Predator and Prey boat classes with sail() stubs

---
+++ src/boat/PredatorBoat.java
@@ -0,0 +1,17 @@
+package boat;
+
+/**
+ * @author David Capper <dmc2@aber.ac.uk>
+ * @version 0.0 (19 August 2013)
+ */
+
+public class PredatorBoat extends Boat {
+
+    public PredatorBoat()
+    {
+        super();
+    }
+
+    @Override
+    public void sail(){}
+}
